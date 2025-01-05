ea0ef4767b801c32da5cb46677d755a87e3a5ec0
Mihail Kolev <kolev210@gmail.com>
2025-01-05 22:18:00 +0200

    rotation done

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ def main():
                 return
         screen.fill((0,0,0))
         player_1.draw(screen)
+        player_1.update(dt)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 if __name__ == "__main__":
