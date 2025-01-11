a66971f658faedee00cab311c10a5084a1063ef6
Mihail Kolev <kolev210@gmail.com>
2025-01-11 11:13:22 +0200

    grupus added

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from player import *
 
 def main():
     pygame.init()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     player_1 = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
@@ -13,8 +16,9 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0))
-        player_1.draw(screen)
-        player_1.update(dt)
+        for thing in updatable:
+            thing.update(dt)
+        drawable.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 if __name__ == "__main__":
