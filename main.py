3b1feabb608ea1c43e51ed72a38e4db37e04236e
Mihail Kolev <kolev210@gmail.com>
2025-01-13 23:23:37 +0200

    Asteroids Part 1 Done

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
-    Asteroid.containers = (asteroids, drawable, updatable)
+    Asteroid.containers = (asteroids, updatable, drawable)
     Player.containers = (updatable, drawable)
-    AsteroidField.containers = (updatable)
+    AsteroidField.containers = updatable
     field = AsteroidField()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
@@ -24,7 +24,8 @@ def main():
         screen.fill((0,0,0))
         for thing in updatable:
             thing.update(dt)
-        drawable.draw(screen)
+        for sprite in drawable:
+            sprite.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 if __name__ == "__main__":
